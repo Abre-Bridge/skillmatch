8b7f478d2bae5af48e06337b32f0a2eb7343ab33
Abre Bridge <abrebridge2628@gmal.com>
2026-06-04 03:55:13 +0100

    add android build workflow

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -6,7 +6,7 @@ const ENV = {
 SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5dGN4eXl0d2VvcnZ0dWJ5b214Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1MTg2ODAsImV4cCI6MjA5NjA5NDY4MH0.AZKArVh1omI5ly0ujOyLzpBaADUxbmc4XzeoQGBkVX8',
   GOOGLE_WEB_CLIENT_ID: '912563311330-glgkqho2tba3rrdqacisk4gfpan3tq7o.apps.googleusercontent.com',
   GOOGLE_ANDROID_CLIENT_ID: '912563311330-eu95vdu0n7o4t5i48ll50lpn2mk3u32r.apps.googleusercontent.com',
-  GOOGLE_IOS_CLIENT_ID: 'your_google_ios_client_id_here',
+  GOOGLE_IOS_CLIENT_ID: 'your_google_ios_client_id_here_',
 
 
 };
